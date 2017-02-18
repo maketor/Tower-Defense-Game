bfad3b9e0fd0e78e6333005cf3293465938735c8
NintendoExaminer <sean.stevens.private@gmail.com>
2017-02-17 19:04:30 -0800

    Assets from after episode 1

---
+++ Tower Defense Game As of Ep 1/Assets/Scripts/Map.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public static Map instance;
+
+
+    public Sprite floor;
+    public Sprite wall;
+
+
+    public int width;
+    public int height;
+
+    Tile[,] tiles;
+
+	// Use this for initialization
+    void Awake () {
+
+        instance = this;
+    }
+
+	void Start () {
+
+        tiles = new Tile[width, height];
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+
+                if(j == 6 || j == 7 || j == 8) {
+
+                    tiles[i, j] = new Tile(i, j, Tile.Type.Floor, floor);
+                    continue;
+                }
+
+
+                if (i == 0 || j == 0 || i == width - 1 || j == height - 1) {
+
+                    tiles[i, j] = new Tile(i, j, Tile.Type.Wall, wall);
+                }
+                else
+                    tiles[i, j] = new Tile(i, j, Tile.Type.Floor, floor);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Tower Defense Game As of Ep 1/Assets/Scripts/Turret.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+    
+    void Start () {
+
+
+    }
+
+    void Awake () {
+
+
+    }
+
+}
